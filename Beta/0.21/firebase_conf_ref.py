cde7f8ac5b1bd02819a5739353d948cd57c619a0
Muhammadali31 <126767627+Muhammadali31@users.noreply.github.com>
2023-10-22 16:23:49 +0100

    Update firebase_conf_ref.py

--- Beta/0.21/firebase_conf_ref.py
+++ Beta/0.21/firebase_conf_ref.py
@@ -1,7 +1,7 @@
-firebaseConf={'apiKey': "AIzaSyC68j6HLD_N_Z2wbgbnSAbi6f8CqLdLhLs",
-    'authDomain': "social-media-clone-85907.firebaseapp.com",
-    'databaseURL': "https://social-media-clone-85907-default-rtdb.europe-west1.firebasedatabase.app",
-    'projectId': "social-media-clone-85907",
-    'storageBucket': "social-media-clone-85907.appspot.com",
-    'messagingSenderId': "929499834534",
-    'appId': "1:929499834534:web:e98171d9cd52edbb432c67"}
+firebaseConf={'apiKey': "XXXXX",
+    'authDomain': "XXXXX",
+    'databaseURL': "XXXXX",
+    'projectId': "XXXXX",
+    'storageBucket': "XXXXX",
+    'messagingSenderId': "XXXXX",
+    'appId': "XXXXX"}
